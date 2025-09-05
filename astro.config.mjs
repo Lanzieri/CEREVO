e78eea28d0156cc78e8beeb5d930c666d5af58b2
Lanzieri <agustin.salgado@dalaincgroup.com>
2025-09-05 20:44:30 +0000

    chore: add tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   site: 'https://lanzieri.github.io',
   base: '/DalaInc/',
-  vite: { plugins: [tailwindcss()] },
+  integrations: [tailwind()],
 })
