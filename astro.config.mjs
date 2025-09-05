e1d7d4c7090d2387fdccf8e007299be57317abfa
Lanzieri <agustin.salgado@dalaincgroup.com>
2025-09-05 20:12:46 +0000

    style: activar Tailwind con global.css e import en layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://lanzieri.github.io',  // tu user de GitHub
-  base: '/DalaInc/',                   // ¡respetá mayúsculas/minúsculas del repo!
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://lanzieri.github.io',
+  base: '/DalaInc/',
+  vite: { plugins: [tailwindcss()] },
 })
