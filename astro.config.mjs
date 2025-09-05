52ef84319aac1ec43a0ad339d21302a5f3b379ec
Lanzieri <agustin.salgado@dalaincgroup.com>
2025-09-05 19:34:56 +0000

    chore: GH Pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  site: 'https://lanzieri.github.io',  // tu user de GitHub
+  base: '/DalaInc/',                   // ¡respetá mayúsculas/minúsculas del repo!
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
